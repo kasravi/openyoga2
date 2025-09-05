a09c6aa6421ab31c78996d50fe173d207444327e
mortezakasravi <morteza.kasravi@danfoss.com>
2025-09-05 19:04:42 +0200

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -14,14 +14,21 @@ export default defineConfig({
       'Cross-Origin-Embedder-Policy': 'require-corp'
     }
   },
+  worker: {
+    format: 'es'
+  },
   build: {
+    target: 'esnext',
     rollupOptions: {
       output: {
-        format: 'esm',
+        // Remove format to let Vite handle it automatically
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
         assetFileNames: 'assets/[name]-[hash][extname]'
       }
     }
+  },
+  esbuild: {
+    target: 'esnext'
   }
 });
